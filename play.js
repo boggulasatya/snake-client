eb8d039a0665accc2c4e3e1ff362b66c303456fa
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-14 02:10:56 -0500

    Using the Input Module in play.js

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const { connect } = require("./client");
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
+const { setupInput } = require("./input");
 
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
-
-const handleUserInput = function () {
-  if(key === "\u0003") {
-    process.exit();
-  }
-};
 console.log("Connecting ...");
 connect();
-
 setupInput();
