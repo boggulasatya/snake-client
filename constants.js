0bd38a8cc78cb65b5a5709ec933676338c92f0e5
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-16 21:41:30 -0500

    modified keys & say messages to be object

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = "localhost";
+const PORT = 50541;
+const moveKeys = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+};
+
+ const sayMessage = {
+  m: 'Say: Hey Snake!',
+  g: 'Say: Have Good game!',
+  h: 'Say: Have fun!'
+};
+
+module.exports = {
+  IP,
+  PORT,
+  moveKeys,
+  sayMessage
+};
