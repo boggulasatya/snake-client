6c46d286e91f2cc430c241a302d0b305710e0e42
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-17 15:37:14 -0500

    Removed empty lines

--- input.js
+++ input.js
@@ -17,15 +17,18 @@ let setupInput = (conn) => {
 const handleUserInput = (key) => {
   if (key === "\u0003") {
     process.exit();
-  
+
   }
+
+  // Using keys to move snake which direction
   const move = moveKeys[key];
-  if(move) {
+  if (move) {
     connection.write(move);
   }
 
+  //Sending messages to server and it will display that text on the screen, next to our snake.
   const message = sayMessage[key];
-  if(message) {
+  if (message) {
     connection.write(message);
   }
 };
