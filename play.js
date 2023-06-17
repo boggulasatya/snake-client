c1272e901ca23be55c4d1a16cdd767a69e8ef1f1
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-16 21:42:02 -0500

    added line3

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 const { connect } = require("./client");
 const { setupInput } = require("./input");
+const { IP, PORT, moveKeys, sayMessage } = require("./constants");
 
 console.log("Connecting ...");
 //pass the object returned from connect into the setupInput function.
